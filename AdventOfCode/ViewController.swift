02e021d5df85f5e6ab9c7fd971a2e0cbc1f777c0
Keith Hunter <hunterkb89@gmail.com>
2018-12-02 15:26:00 -0500

    Initial Commit

---
+++ AdventOfCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdventOfCode
+//
+//  Created by Keith Hunter on 12/2/18.
+//  Copyright © 2018 Keith Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
